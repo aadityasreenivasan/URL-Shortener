2e25f34d27eee95dd541c5bb0cea9a3e14171c67
aadityasreenivasan <104477298+aadityasreenivasan@users.noreply.github.com>
2023-05-20 14:45:33 +0530

    Update shortURL.py

--- shortURL.py
+++ shortURL.py
@@ -9,3 +9,16 @@ try:
 except ImportError:
 	from urllib2 import urlopen
 import sys
+
+def make_tiny(url):
+	request_url = ('http://tinyurl.com/api-create.php?' + 
+	urlencode({'url':url}))
+	with contextlib.closing(urlopen(request_url)) as response:
+		return response.read().decode('utf-8')
+
+def main():
+	for tinyurl in map(make_tiny, sys.argv[1:]):
+		print(tinyurl)
+
+if __name__ == '__main__':
+	main()
